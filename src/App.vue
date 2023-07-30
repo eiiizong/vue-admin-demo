74b5bf28f44fe38d533ff89a518ad8a7b3dc6392
eiiizong <eiiizong@163.com>
2023-07-30 16:09:01 +0800

    local

--- src/App.vue
+++ src/App.vue
@@ -1,72 +1,17 @@
 <template>
-  <RouterView />
+  <el-config-provider>
+    <router-view />
+  </el-config-provider>
 </template>
 
-<script setup lang="ts">
-import {  RouterView } from 'vue-router'
+<script lang="ts" setup>
+import { ElConfigProvider } from 'element-plus'
+// import { computed } from 'vue'
+// import en from 'element-plus/lib/locale/lang/en'
+// import { ReDialog } from '@/components/ReDialog'
+// import zhCn from 'element-plus/lib/locale/lang/zh-cn'
 
+// const currentLocale = computed(() => {
+//   return zhCn
+// })
 </script>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
