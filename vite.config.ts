2565054e6fb7339c23847d12550c27f2e6ee0aac
eiiizong <eiiizong@163.com>
2023-07-30 13:30:04 +0800

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,20 @@
 import { fileURLToPath, URL } from 'node:url'
+import dayjs from 'dayjs'
+import pkg from './package.json'
 
 import { loadEnv } from 'vite'
 
 import { wrapperEnv } from './build/utils'
 import { createVitePlugins } from './build/vite/plugins'
+import { createProxy } from './build/vite/proxy'
 
 import type { UserConfigExport, ConfigEnv } from 'vite'
 
+const { dependencies, devDependencies, name, version } = pkg
+
 const __APP_INFO__ = {
-  pkg: {},
-  lastBuildTime: ''
+  pkg: { dependencies, devDependencies, name, version },
+  lastBuildTime: dayjs().format('YYYY-MM-DD HH:mm:ss')
 }
 
 // https://vitejs.dev/config/
@@ -21,6 +26,8 @@ export default ({ command, mode }: ConfigEnv): UserConfigExport => {
   // 将字符串转化布尔类型
   const viteEnv = wrapperEnv(env)
 
+  const { VITE_PORT, VITE_DROP_CONSOLE, VITE_PROXY } = viteEnv
+
   // 是否为生产环境
   const isBuild = command === 'build'
 
@@ -30,6 +37,15 @@ export default ({ command, mode }: ConfigEnv): UserConfigExport => {
       alias: {
         '@': fileURLToPath(new URL('./src', import.meta.url))
       }
+    },
+    server: {
+      https: isBuild,
+      host: true,
+      port: VITE_PORT,
+      proxy: createProxy(VITE_PROXY)
+    },
+    esbuild: {
+      pure: VITE_DROP_CONSOLE ? ['console.log', 'debugger'] : []
     },
     define: {
       __INTLIFY_PROD_DEVTOOLS__: false,
