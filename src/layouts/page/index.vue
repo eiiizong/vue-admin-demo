03c63f10a6af09ba07bc0b1bbe9c65222c408bf5
eiiizong <eiiizong@163.com>
2023-07-31 16:42:27 +0800

    piniaPluginPersistedstate

--- src/layouts/page/index.vue
+++ src/layouts/page/index.vue
@@ -1,4 +1,19 @@
 <template>
-  <div>page</div>
+  <RouterView>
+    <template #default="{ Component, route }">
+      <transition name="fade" mode="out-in" appear>
+        <keep-alive v-if="openCache">
+          <component :is="Component" :key="route.fullPath" />
+        </keep-alive>
+        <div v-else>
+          <component :is="Component" :key="route.fullPath" />
+        </div>
+      </transition>
+    </template>
+  </RouterView>
 </template>
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+const openCache = ref(false)
+</script>
--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,8 +1,12 @@
-import { createPinia } from 'pinia'
-
 import type { App } from 'vue'
 
+import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+
 const store = createPinia()
+
+// 引入数据持久化插件
+store.use(piniaPluginPersistedstate)
 
 /**
  * 配置 store
