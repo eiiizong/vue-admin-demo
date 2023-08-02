f50a893950761f11ef63e37cb645942cf86e4b7d
eiiizong <eiiizong@163.com>
2023-08-02 10:48:50 +0800

    userSettings

--- src/stores/modules/useStroeUserSettings.ts
+++ src/stores/modules/useStroeUserSettings.ts
@@ -1,12 +1,28 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 
+import type { StroeUserSettings } from '@/stores/types'
+
 const useStroeUserSettings = defineStore('stroeUserSettings', () => {
-  const userSettings = ref({})
+  const userSettings = ref<StroeUserSettings>({
+    menu: {
+      mode: 'vertical',
+      width: '260px',
+      isCollapse: false
+    },
+    theme: {
+      color: '#f00',
+      isDark: false
+    },
+    language: 'zh-cn',
+    showLogo: true,
+    showTabs: true,
+    showBreadcrumb: true
+  })
 
   const getUserSettings = computed(() => userSettings.value)
 
-  function updateUserSettings(data: any) {
+  function updateUserSettings(data: StroeUserSettings) {
     userSettings.value = { ...userSettings.value, ...data }
   }
 
--- src/stores/types.ts
+++ src/stores/types.ts
@@ -1,7 +1,51 @@
 /**
- * 用户设置
+ * 用户设置 store
  */
 export interface StroeUserSettings {
-  menu: {}
-  themeColor: string
+  /**
+   * 菜单配置
+   */
+  menu: {
+    /**
+     * 菜单展示模式 horizontal 横向  vertical 纵向
+     */
+    mode: 'horizontal' | 'vertical'
+    /**
+     * 菜单宽度
+     */
+    width: string
+    /**
+     * 是否水平折叠收起菜单（仅在 mode 为 vertical 时可用）
+     */
+    isCollapse: boolean
+  }
+  /**
+   * 主题配置
+   */
+  theme: {
+    /**
+     * 主题色
+     */
+    color: string
+    /**
+     * 是否为暗黑模式
+     */
+    isDark: boolean
+  }
+  /**
+   * 语言
+   */
+  language: 'zh-cn'
+  /**
+   * 是否显示logo
+   */
+  showLogo: boolean
+  /**
+   * 是否显示标签
+   */
+  showTabs: boolean
+  /**
+   * 是否显示面包屑
+   */
+  showBreadcrumb: boolean
 }
