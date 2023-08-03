7fe5bd0a69c1f73fa99e33bd4462a1f1bfc3aa1a
eiiizong <eiiizong@163.com>
2023-08-03 10:12:42 +0800

    stylelint

--- src/views/workbench/index.vue
+++ src/views/workbench/index.vue
@@ -1,4 +1,10 @@
 <template>
-  <div class="div">工作台</div>
+  <div class="workbench full">工作台</div>
 </template>
 <script></script>
+
+<style lang="scss" scoped>
+.workbench {
+  @apply bg-yellow-200;
+}
+</style>
