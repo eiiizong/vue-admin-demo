8be79c97e9e1f1702723aa245a5fe9ef20f12241
eiiizong <eiiizong@163.com>
2023-08-10 16:23:16 +0800

    router

--- src/router/constant.ts
+++ src/router/constant.ts
@@ -14,11 +14,11 @@ export const LAYOUT = () => import('@/layouts/default/index.vue')
 /**
  * @description: parent-layout
  */
-export const getParentLayout = (_name?: string) => {
-  return () =>
-    new Promise((resolve) => {
-      resolve({
-        name: _name || PARENT_LAYOUT_NAME
-      })
-    })
-}
+// export const getParentLayout = (_name?: string) => {
+//   return () =>
+//     new Promise((resolve) => {
+//       resolve({
+//         name: _name || PARENT_LAYOUT_NAME
+//       })
+//     })
+// }
